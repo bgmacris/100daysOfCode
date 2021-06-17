9324175e2d8d055ca24deeb6e4ba813c6fa67535
PyBgX <bgmacris9999@gmail.com>
2021-06-17 22:21:42 +0200

    1r dia
    Api idecast.cat

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from controller import Idescat
 idescat = Idescat()
 
 id_senia = idescat.get_id_pob('senia')
-print(id_senia)
+if 'id' in id_senia:
+    id_senia = id_senia['id']
+    datos_pob = idescat.get_info_pob(id_senia)
+    print(datos_pob)
+else:
+    print("No se ha encontrado la poblacion")
 
 # Datos superficie
 # datos_senia['gg']['g'][0]['tt']['t']['ff']['f']
