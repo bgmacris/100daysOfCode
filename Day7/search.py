6eb0ee47453e10f855dab8c78bba7e4c602867dd
PyBgX <bgmacris9999@gmail.com>
2021-06-23 18:33:11 +0200

    Day 7
    Algoritmo de calculo de rutas(Busqueda en aplietud), api para solicitar busquedas solo en las conexiones permititdas.

---
+++ Day7/search.py
@@ -0,0 +1,56 @@
+from arbol import Nodo
+
+def busqueda_amplietud(conexiones, estado_inicial, objetivo):
+    nodos_visitados = []
+    nodos_frontera = []
+    nodo_inicial = Nodo(estado_inicial)
+    nodos_frontera.append(nodo_inicial)
+
+    while len(nodos_frontera):
+        nodo = nodos_frontera.pop(0)
+        nodos_visitados.append(nodo)
+
+        if nodo.get_datos() == objetivo:
+            return nodo
+        else:
+            datos_nodo = nodo.get_datos()
+            hijos = []
+            for hijo in conexiones[datos_nodo]:
+                hijo = Nodo(hijo)
+                
+                hijos.append(hijo)
+                if not hijo.en_lista(nodos_frontera) and not hijo.en_lista(nodos_visitados):
+                    nodos_frontera.append(hijo)
+
+        nodo.set_hijos(hijos)
+                
+
+
+def search_ruta(estado_inicial, objetivo):
+    conexiones = {
+            'malaga': {'salamanca', 'madrid', 'barcelona'},
+            'sevilla': {'santiago', 'madrid'},
+            'granada': {'valencia'},
+            'valencia': {'barcelona', 'granada'},
+            'madrid': {'salamanca', 'sevilla', 'malaga', 'barcelona', 'santander'},
+            'barcelona': {'zaragoza', 'santiago', 'madrid', 'malaga', 'valencia'},
+            'salamanca': {'malaga', 'madrid'},
+            'santiago': {'sevilla', 'santander', 'barcelona'},
+            'santander': {'santiago', 'madrid'},
+            'zaragoza': {'barcelona'}
+        }
+
+    estado_inicial = estado_inicial
+    objetivo = objetivo
+
+    resultado = []
+    nodo_solucion = busqueda_amplietud(conexiones, estado_inicial, objetivo)
+    nodo = nodo_solucion
+    print(nodo.get_padre(),"X")
+    while nodo.get_padre() != None: 
+        resultado.append(nodo.get_datos())
+        nodo = nodo.get_padre()
+    
+    resultado.append(estado_inicial)
+    print(resultado[::-1])
+    return(resultado[::-1])
